7e08ab783deed12849077c9a6f383088a8bd0344
Sanika Jain <jsanika446@gmail.com>
2026-05-31 22:18:05 +0530

    Remove Linked list Elements

---
+++ LinkedList/Remove_Lined_list_elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+       if(!head){
+            return head;
+        }
+        
+        ListNode * h = nullptr;
+        ListNode *ret = nullptr;
+        while(head){
+            if(head->val != val){
+                if(!h){
+                    h = new ListNode(head->val);
+                    ret = h;
+                }else{
+                    h->next = new ListNode(head->val);
+                    h = h->next;
+                }    
+            }
+            head = head->next;
+        }
+        return ret;  
+    }
+};
